32402efca369c526532f6fa0d63904456c347ad6
nhatlinh8816 <dangnhatlinh0213@gmail.com>
2021-06-04 11:13:27 +0700

    update_thuchanh_baitap_0306

---
+++ src/_03_array/exercise/MinOfArray.java
@@ -0,0 +1,25 @@
+package _03_array.exercise;
+
+import java.util.Scanner;
+
+public class MinOfArray {
+    public static void main(String[] args) {
+        int m,i; //khai báo số nguyên m,i;
+        Scanner input = new Scanner(System.in);
+        do {
+            System.out.println("Nhập độ dài mảng");
+            m = input.nextInt();
+        }while (m<=0);
+        int[]array = new int[m];   //khai báo mảng array có độ dài m;
+        for (i=0;i<m;i++){
+            System.out.println("Nhập phần tử tại vị trí "+i+":");
+            array[i] = input.nextInt();
+        }
+        int min = array[0];
+        for (i=1;i<m;i++){
+            if (array[i]<min){
+                min = array[i];
+            }
+        }System.out.println("Min of array is "+min);
+    }
+}
