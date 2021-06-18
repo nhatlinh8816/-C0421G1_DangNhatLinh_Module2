237ffd4c1ff82ad4b9d43a5fc95e76fd58c367b6
nhatlinh8816 <dangnhatlinh0213@gmail.com>
2021-06-18 16:59:49 +0700

    update_thuchanh_baitap_1806

--- src/_11_dsa_stack_queue/exercise/TreeMap.java
+++ src/_11_dsa_stack_queue/exercise/TreeMap.java
@@ -4,17 +4,25 @@ import java.util.Map;
 
 public class TreeMap {
     public static void main(String[] args) {
-        String string = "Hứa thật nhiều thất hứa thật nhiều nhiều hứa";
+        String string = "a b  a";
         String string1 = string.toLowerCase();
         System.out.println(string1);
         String[]word = string1.split(" ");
+        for (String w:word){
+            System.out.println(w);
+        }
         Map<String,Integer>map = new java.util.TreeMap<String,Integer>();
+
         for(int i = 0; i<word.length;i++){
-            if (!map.containsKey(word[i])){
-                map.put(word[i],1);
+            if (word[i].equals("")) {
+                continue;
             }else {
-                int a = map.get(word[i])+1;
-                map.put(word[i],a);
+                if (!map.containsKey(word[i])) {
+                    map.put(word[i], 1);
+                } else {
+                    int a = map.get(word[i]) + 1;
+                    map.put(word[i], a);
+                }
             }
         }
         System.out.println(map);
